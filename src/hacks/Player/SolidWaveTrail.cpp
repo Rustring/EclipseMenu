62545831c3f61903388f11de7fae399c870d2ef4
Rustring <155897302+Rustring@users.noreply.github.com>
2024-08-08 09:50:06 +0000

    fixes x1

--- src/hacks/Player/SolidWaveTrail.cpp
+++ src/hacks/Player/SolidWaveTrail.cpp
@@ -21,8 +21,8 @@ namespace eclipse::hacks::Player {
 
     REGISTER_HACK(SolidWaveTrail)
 
-   class $modify(CCDrawNode) {    // Yes this is copied from Prism Menu I'm stupid to write it myself
-       bool drawPolygon(CCPoint *p0, unsigned int p1, const ccColor4F &p2, float p3, const ccColor4F &p4) {
+   class $modify(cocos2d::CCDrawNode) {    // Yes this is copied from Prism Menu I'm stupid to write it myself
+       bool drawPolygon(cocos2d::CCPoint *p0, unsigned int p1, const ccColor4F &p2, float p3, const cocos2d::ccColor4F &p4) {
            if (!config::get<bool>("player.solidwavetrail", true))
              return CCDrawNode::drawPolygon(p0,p1,p2,p3,p4);
            if (p2.r == 1.F && p2.g == 1.F && p2.b == 1.F && p2.a != 1.F) 
