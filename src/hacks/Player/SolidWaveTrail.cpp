7e209411eb90ca800e35b4ebde4254af23977e0f
Rustring <155897302+Rustring@users.noreply.github.com>
2024-08-09 11:57:30 +0000

    hopefully this works

--- src/hacks/Player/SolidWaveTrail.cpp
+++ src/hacks/Player/SolidWaveTrail.cpp
@@ -22,28 +22,16 @@ namespace eclipse::hacks::Player {
 
    REGISTER_HACK(SolidWaveTrail)
 
-
-   class $modify(cocos2d::CCDrawNode) {    // The actual code behind the hack
-       
-        bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor)    
-       
+   class $modify(cocos2d::CCDrawNode) {    // The actual hack code
+       bool drawPolygon(cocos2d::CCPoint *p0, unsigned int p1, const cocos2d::ccColor4F &p2, float p3, const cocos2d::ccColor4F &p4) {
            if (!config::get<bool>("player.solidwavetrail", true))
-            {
-                if (fillColor.r >= 1.0f && fillColor.g >= 1.0f && fillColor.b >= 1.0f && this->getColor() != ccc3(255, 255, 255))
-                    return true;
-
-                if (this->getTag() != 1)
-                {
-                    this->setTag(1);
-                    this->setBlendFunc(CCSprite::create()->getBlendFunc());
-                }
-
-                this->setZOrder(-1);
-            }
-        }
-
-        return CCDrawNode::drawPolygon(verts, count, fillColor, borderWidth, borderColor);
-     }
+             return CCDrawNode::drawPolygon(p0,p1,p2,p3,p4);
+           if (p2.r == 1.F && p2.g == 1.F && p2.b == 1.F && p2.a != 1.F) 
+             return true; 
+           this->setBlendFunc(CCSprite::create()->getBlendFunc());
+           this->setZOrder(-1); 
+           return CCDrawNode::drawPolygon(p0,p1,p2,p3,p4);
+       }
    };
 
 }
